8aceb4b1e5c584cb74c02f508f6b5c911a07ce1f
Jeffrey <golettoo@gmail.com>
2012-11-17 22:31:29 +0800

    use aws s3

--- src/webapp/settings.py
+++ src/webapp/settings.py
@@ -46,14 +46,15 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+ASSETS_URL = 'https://%s.s3.amazonaws.com/' %(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'media')
+MEDIA_ROOT = ASSETS_URL + 'site_media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/site_media/media/'
+MEDIA_URL = ASSETS_URL + 'site_media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -163,6 +164,11 @@ LOGGING = {
         },
     }
 }
+
+DEFAULT_FILE_STORAGE = os.environ.get('DEFAULT_FILE_STORAGE')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 try:
     from local_settings import *
