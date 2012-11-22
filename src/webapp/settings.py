aa4f3a22aa14cdcb8d1b41c903e15a665550cc46
Jeffrey <golettoo@gmail.com>
2012-11-22 19:52:34 +0800

    update aws s3 to http

--- src/webapp/settings.py
+++ src/webapp/settings.py
@@ -46,7 +46,7 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-ASSETS_URL = 'https://%s.s3.amazonaws.com/' %(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
+ASSETS_URL = 'http://%s.s3.amazonaws.com/' %(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ASSETS_URL + 'site_media/'
